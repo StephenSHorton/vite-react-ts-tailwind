7536b0a8260e00fc6fa54174ec2952bcef8db6dd
StephenSHorton <4stephenhorton@gmail.com>
2022-10-04 13:12:58 -0600

    first commit

---
+++ prettier.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: [require('prettier-plugin-tailwindcss')],
+  tailwindConfig: './tailwind.config.cjs',
+  semi: false,
+  singleQuote: true,
+  useTabs: false,
+  tabWidth: 2,
+}
